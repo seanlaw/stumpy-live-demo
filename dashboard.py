eff640938d106aa61d1841f4fccbba85e6924a22
Sean Law <seanmylaw@gmail.com>
2020-05-07 11:06:34 -0400

    Rearranged the buttons

--- dashboard.py
+++ dashboard.py
@@ -418,7 +418,7 @@ class DASHBOARD():
             #[self.slider, self.txt_inp],
             [self.slider],
             #[self.sms_inp, self.play_btn, self.pattern_btn, self.match_btn, self.gauge_btn, self.reset_btn]], 
-            [self.play_btn, self.pattern_btn, self.match_btn, self.logo_div]],
+            [self.pattern_btn, self.match_btn, self.play_btn, self.logo_div]],
             sizing_mode=self.sizing_mode)
 
         return l
